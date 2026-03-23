07b7216c70945285ac2b44d5cb7ea5e616c222d9
Minh Tri <nguyenminhtri1808t@gmail.com>
2026-03-23 12:14:34 +0700

    refactor(dto): change name file

--- src/modules/auth/application/dto/email-register.dto.ts
+++
@@ -1,6 +0,0 @@
-import { EmailValidator } from '@modules/auth/presentation/decorator/email-validator.decorator';
-
-export class SendEmailVerificationOtpDTO {
-  @EmailValidator()
-  email: string;
-}
