729a50d058c5002a899cb6ec7e3d2a2780c19cee
Minh Tri <nguyenminhtri1808t@gmail.com>
2026-05-07 19:22:31 +0700

    refactor(exception): add case unauthorization in global exception filter

--- src/shared/presentation/filters/global-exception.filter.ts
+++ src/shared/presentation/filters/global-exception.filter.ts
@@ -3,6 +3,7 @@ import {
   Catch,
   ExceptionFilter,
   HttpStatus,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { Response } from 'express';
 import { RequestBodyEmptyException } from '../exceptions';
@@ -21,6 +22,22 @@ export class GlobalExceptionFilter implements ExceptionFilter {
   catch(exception: any, host: ArgumentsHost): void {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
+
+    if (exception instanceof UnauthorizedException) {
+      const status = exception.getStatus();
+
+      const responseBody: ApiError = {
+        success: false,
+        error: {
+          code: 'UN_AUTHORIZATION',
+          message: exception.message,
+          details: null,
+        },
+      };
+
+      response.status(status).json(responseBody);
+      return;
+    }
 
     if (exception instanceof RequestBodyEmptyException) {
       const status = exception.getStatus();
