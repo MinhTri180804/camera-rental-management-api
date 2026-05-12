45e3957383f88db04c2b4378120096d280917880
Minh Tri <nguyenminhtri1808t@gmail.com>
2026-05-12 19:52:07 +0700

    feat(utils): implement normalized name function

---
+++ src/common/utils/index.ts
@@ -0,0 +1,12 @@
+export { normalizedName } from './normalized-name.util';
+export { cleanObject } from './clean-object.util';
+export { getEnvFilePath } from './get-env-file-path.util';
+export { hashPassword, comparePassword } from './hash-password.util';
+export { generateOTP, hashOtp, verifyOtp } from './otp.utils';
+export { registerEnv } from './register-env.util';
+export {
+  secondToMs,
+  msToSecondFloor,
+  msToSecondCell,
+  msToSecond,
+} from './time.util';
